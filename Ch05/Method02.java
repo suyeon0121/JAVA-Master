f138e0fcb9f7817ec8b261abb5c11c1164b1c116
suyeon <130628673+suyeon0121@users.noreply.github.com>
2023-08-05 14:35:34 +0900

    Add files via upload

---
+++ Ch05/Method02.java
@@ -0,0 +1,16 @@
+package Ch05;
+
+public class Method02 {
+    public static int div() {
+        int a = 10, b = 5;
+        int result = a / b;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int num = div();
+        System.out.println(num);
+    }
+    
+}
