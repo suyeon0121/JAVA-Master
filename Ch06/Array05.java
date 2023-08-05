caee5b93d2c635757b7aaad4a2a89a3412e3f275
suyeon <130628673+suyeon0121@users.noreply.github.com>
2023-08-05 14:35:50 +0900

    Add files via upload

---
+++ Ch06/Array05.java
@@ -0,0 +1,12 @@
+package Ch06;
+
+public class Array05 {
+    public static void main(String[] args) {
+        String s1 = "Java Programming";
+        String s2 = new String("Java Programming");
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+    
+}
