a95504ae9b4ea2aa72386bde325d41eebd0610b1
suyeon <130628673+suyeon0121@users.noreply.github.com>
2023-08-14 10:24:06 +0900

    Add files via upload

---
+++ Ch13/Collection07.java
@@ -0,0 +1,36 @@
+package Ch13;
+
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+public class Collection07 {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> str = new TreeMap<Integer, String>();
+
+        str.put(1, "A");
+        str.put(2, "B");
+        str.put(3, "C");
+
+        System.out.println(str);
+
+        System.out.print("Keys : ");
+        for (Integer key : str.keySet()) {
+            System.out.print(key);
+            System.out.print(", ");
+        }
+
+        System.out.println();
+        System.out.print("Values : ");
+        for (String value : str.values()) {
+            System.out.print(value);
+            System.out.print(", ");
+        }  
+        
+        System.out.println();
+        System.out.print("Keys=Values : ");
+        for (Entry<Integer, String> entry : str.entrySet()) {
+            System.out.print(entry);
+            System.out.print(", ");
+        }
+    }
+}
